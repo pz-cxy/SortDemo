7ea0a1312ffdd9734be37191dc8908695fdb051a
pz-cxy <m17324426187@163.com>
2020-04-01 10:42:39 +0800

    基数排序的算法实现

---
+++ src/chpz/RadixSort.java
@@ -0,0 +1,47 @@
+package chpz;
+
+public class RadixSort {
+    //找出一个数组的最大值
+    public static int findMax(int[] arrays, int L, int R) {
+
+        //如果该数组只有一个数，那么最大的就是该数组第一个值了
+        if (L == R) {
+            return arrays[L];
+        } else {
+
+            int a = arrays[L];
+            int b = findMax(arrays, L + 1, R);//找出整体的最大值
+
+            if (a > b) {
+                return a;
+            } else {
+                return b;
+            }
+        }
+    }
+    public static void sort(int[] arrays){
+        int max = findMax(arrays, 0, arrays.length-1);
+        //需要遍历的次数由数组最大值的位数来决定
+        for (int i=1;max/i > 0;i=i*10) {
+            int[][] buckets = new int[arrays.length][10];
+            //获取每一位数字(个、十、百、千位...分配到桶子里)
+            for (int j = 0; j < arrays.length; j++) {
+                int num = (arrays[j] / i) % 10;
+                //将其放入桶子里
+                buckets[j][num] = arrays[j];
+            }
+            //回收桶子里的元素
+            int k = 0;
+            //有10个桶
+            for (int j = 0; j < 10; j++) {
+                //对每个桶子里的元素进行回收
+                for (int l = 0; l < arrays.length; l++) {
+                    //如果桶子里面有元素就回收(数据初始化会为0)
+                    if (buckets[l][j] != 0) {
+                        arrays[k++] = buckets[l][j];
+                    }
+                }
+            }
+        }
+    }
+}
